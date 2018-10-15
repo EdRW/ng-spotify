6e26480afd962deab5168a99ce28d2f63313cc82
Edmund Wright <nekurasky@gmail.com>
2018-10-15 00:44:06 -0500

    Added the search component by api query is broken

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,13 @@ import { ArtistComponent } from './artist/artist.component';
 import { AlbumComponent } from './album/album.component';
 import { TrackComponent } from './track/track.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: 'search', component: SearchComponent },
+  { path: 'artists/:id', component: ArtistComponent },
+  { path: 'tracks/:id', component: TrackComponent },
+  { path: 'albums/:id', component: AlbumComponent },
+];
 
 @NgModule({
   declarations: [

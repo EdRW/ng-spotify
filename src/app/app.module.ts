9715a94c8ea8e6d477325ee3bacaef75d37bf7f5
Edmund Wright <nekurasky@gmail.com>
2018-10-14 23:04:28 -0500

    initial commit: generated components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ArtistComponent } from './artist/artist.component';
+import { AlbumComponent } from './album/album.component';
+import { TrackComponent } from './track/track.component';
+
+const routes: Routes = [];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ArtistComponent,
+    AlbumComponent,
+    TrackComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
